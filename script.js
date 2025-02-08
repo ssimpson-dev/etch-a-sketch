0f0c01778fab1fc6b7442d8950ee0a29e9d07be2
Simmington <simmington96@gmail.com>
2025-02-08 22:01:56 +0000

    add javascript mouseover colour

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
 const container = document.querySelector("#grid-container");
 
-let button = document.createElement("button")
-document.body.appendChild(button)
-button.textContent = "Reset grid"
+let button = document.createElement("button");
+document.body.appendChild(button);
+button.textContent = "Reset grid";
 
 button.addEventListener("click", function () {
-    const userInput = prompt("Enter the number of squares per side (max 100).");
+  const userInput = prompt("Enter the number of squares per side (max 100).");
 
-    const gridSize = Number(userInput);
+  const gridSize = Number(userInput);
 
-    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
-        alert("Please enter a number between 1 and 100.");
-        return;
-    }
+  if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+    alert("Please enter a number between 1 and 100.");
+    return;
+  }
 
-    createGrid(gridSize)
+  createGrid(gridSize);
 });
 
-function createGrid (gridSize){
-    container.innerHTML = "";
+function createGrid(gridSize) {
+  container.innerHTML = "";
 
-    const squareSize = 960 / gridSize;
+  const squareSize = 960 / gridSize;
 
-    for (let i = 0; i < gridSize * gridSize; i++){
-        const square = document.createElement("div");
-        square.style.width = `${squareSize}px`;
-        square.style.height = `${squareSize}px`;
-        container.appendChild(square);
-}
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
+    container.appendChild(square);
+
+    square.addEventListener("mouseover", function () {
+      square.style.backgroundColor = "purple";
+    });
+  }
 }
 
 createGrid(16);
