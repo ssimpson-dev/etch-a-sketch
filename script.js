89fffc6a2ba565804062c7900661a9bb2fdb29a5
Simmington <simmington96@gmail.com>
2025-02-08 21:44:46 +0000

    etch-a-sketch complete

---
+++ script.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#grid-container");
+
+let button = document.createElement("button")
+document.body.appendChild(button)
+button.textContent = "Reset grid"
+
+button.addEventListener("click", function () {
+    const userInput = prompt("Enter the number of squares per side (max 100).");
+
+    const gridSize = Number(userInput);
+
+    if (isNaN(gridSize) || gridSize < 1 || gridSize > 100) {
+        alert("Please enter a number between 1 and 100.");
+        return;
+    }
+
+    createGrid(gridSize)
+});
+
+function createGrid (gridSize){
+    container.innerHTML = "";
+
+    const squareSize = 960 / gridSize;
+
+    for (let i = 0; i < gridSize * gridSize; i++){
+        const square = document.createElement("div");
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        container.appendChild(square);
+}
+}
+
+createGrid(16);
